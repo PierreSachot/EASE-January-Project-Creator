c7424565bb1e825dddc013d8d57834f5661ec6ba
Pierre Sachot <sachot.pierre@laposte.net>
2017-05-18 14:59:32 +0100

    Adding createJavaProject()

--- src/JPCreator.java
+++ src/JPCreator.java
@@ -1,4 +1,56 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.LibraryLocation;
 
 public class JPCreator {
+
+	public IJavaProject createJavaProject(String projectName) {
+		IProgressMonitor progressMonitor = new NullProgressMonitor();
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(projectName);
+		IJavaProject javaProject = null;
+		try {
+			project.create(progressMonitor);
+			project.open(progressMonitor);
+
+			// Creating JavaProject
+			IProjectDescription description = project.getDescription();
+			String[] natures = description.getNatureIds();
+			String[] newNatures = new String[natures.length + 1];
+			System.arraycopy(natures, 0, newNatures, 0, natures.length);
+			newNatures[natures.length] = JavaCore.NATURE_ID;
+			description.setNatureIds(newNatures);
+			project.setDescription(description, progressMonitor);
+			javaProject = JavaCore.create(project);
+
+			// Adding dependencies
+			Set<IClasspathEntry> entries = new HashSet<IClasspathEntry>();
+			entries.addAll(Arrays.asList(javaProject.getRawClasspath()));
+			IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
+			LibraryLocation[] locations = JavaRuntime.getLibraryLocations(vmInstall);
+			for (LibraryLocation element : locations) {
+				entries.add(JavaCore.newLibraryEntry(element.getSystemLibraryPath(), null, null));
+			}
+			javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), progressMonitor);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return javaProject;
+	}
 
 }
